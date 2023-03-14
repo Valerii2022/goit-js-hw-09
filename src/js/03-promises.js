a108b2187c3c238093c7fc16bd014d14f167ef78
Valerii Pometun <vmpometun@gmail.com>
2023-03-14 23:42:33 +0200

    work with task#3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,49 +3,40 @@ import Notiflix from 'notiflix';
 const refs = {
   form: document.querySelector('.form'),
 };
-
 refs.form.addEventListener('submit', createPromise);
 
 let timerId = null;
 let position = 0;
-let step = 0;
-let delay = 0;
-let amount = 0;
-let currentDelay = 0;
-
-
 
 function createPromise(event) {
   event.preventDefault();
-  amount = Number(refs.form.elements.amount.value);
-  delay = Number(refs.form.elements.delay.value);
-  step = Number(refs.form.elements.step.value);
-  
-  setTimeout(() => {
-currentDelay = delay + step;
-    timerId = setInterval(() => {
-    
-    const shouldResolve = Math.random() > 0.3;
+  const amount = Number(refs.form.elements.amount.value);
+  let delay = Number(refs.form.elements.delay.value);
+  let step = 0;
+
+  timerId = setInterval(() => {
     position += 1;
+    step = Number(refs.form.elements.step.value);
     delay += step;
-    if (shouldResolve) {
-      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    } else {
-      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    }
-    if (position === amount) {
-      clearInterval(timerId);
-    }
-    }, currentDelay)
-    
-  },delay)
-  
-  
+
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+      if (position === amount) {
+        position = 0;
+        clearInterval(timerId);
+        return;
+      }
+    });
+  }, delay + step);
 }
 
-
-
-
-
+// timerId = setInterval(() => {}, delay + step);
