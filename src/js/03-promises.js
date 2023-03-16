551685630574a58b457cd2f50cda6947b01604f7
Valerii Pometun <vmpometun@gmail.com>
2023-03-17 01:25:07 +0200

    almoust done task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,81 +4,49 @@ const refs = {
   form: document.querySelector('.form'),
 };
 
-refs.form.addEventListener('submit', createPromise);
-
-// let timerId = null;
-// let position = 10;
-// const delay = Number(refs.form.elements.delay.value);
-
-// function createPromise() {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     if (shouldResolve) {
-//       resolve({ position, delay });
-//     }
-//     reject({ position, delay });
-//   }, delay);
-// }
-
-// createPromise()
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+refs.form.addEventListener('submit', handleinputSubmit);
 
 let timerId = null;
+let position = 0;
 
-  let position = 0;
-  const delay = 1000;
-  const amount = 5;
+function handleinputSubmit(event) {
+  event.preventDefault();
+  let delay = Number(refs.form.elements.delay.value);
+  const amount = Number(refs.form.elements.amount.value);
+  const step = Number(refs.form.elements.step.value);
 
+  setTimeout(() => {
+    timerId = setInterval(() => {
+      position += 1;
 
-const p = new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `Fulfilled promise ${position} in ${delay}ms`
+          );
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
 
+      if (position == amount) {
+        clearInterval(timerId);
+      }
 
-  setInterval(() => {
-    position += 1;
- if (shouldResolve) {
+      delay += step;
+    }, step);
+  }, delay);
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
       resolve({ position, delay });
     } else {
       reject({ position, delay });
     }
-  }, delay)
-   
-    
-
-
-});
-
-function createPromise(event) {
-  event.preventDefault();
-
-
+  });
 }
-
-
-  const step = 500;
-
-
-   timerId = setInterval(() => {
-    
-
-  p.then(({ position, delay }) => {
-  Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-}).catch(({ position, delay }) => {
-  Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-});
-  }, step);
-
-if (position === amount) {
-      clearInterval(timerId);
-    }
-  
-  
